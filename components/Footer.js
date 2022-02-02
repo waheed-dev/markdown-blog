0c96ea22d435324747c6f0e4ce04a743de74471e
abdul waheed <a.waheed3738@gmail.com>
2022-02-02 21:53:20 +0500

    building footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,5 @@
 import {Box, Flex, Heading, Text, Link, Button} from "@chakra-ui/react";
-import {AiFillGithub, AiFillLinkedin, AiOutlineTwitter} from "react-icons/ai";
+import {AiFillGithub, AiFillLinkedin, AiOutlineTwitter, AiOutlineWarning} from "react-icons/ai";
 import {BiLinkExternal} from "react-icons/bi";
 
 const Footer = () => {
@@ -10,7 +10,7 @@ const Footer = () => {
     }
     return (
         <Box as={'footer'}>
-        <Flex justifyContent={'center'} pb={'10'}>
+        <Flex justifyContent={'center'} pb={'8'} mt={'4'}>
             <Box display={'flex'} gridGap={'6'}>
                 <AiFillGithub size={'25'}/>
                 <AiOutlineTwitter size={'25'} />
@@ -19,15 +19,16 @@ const Footer = () => {
         </Flex>
             <Box textAlign={'center'} pb={'12'}>
                 <Text fontSize={'sm'} mb={'2'}>developed and designed by Abdul Waheed</Text>
-                <Box display={'flex'} justifyContent={'center'} gap={'1'} alignItems={'center'} mb={'2'}>
-                <Text fontSize={'xs'}>Made with <Link color={'blue.500'} fontSize={'sm'} href={'https://chakra-ui.com/'} isExternal>
-                    Chakra Ui
-                </Link>
-                </Text>
-                   <BiLinkExternal/>
-                </Box>
                 <Text fontStyle={'italic'} color={'gray.500'} fontSize={'sm'}>© 2022,All rights reserved.</Text>
                 <Button bgColor={'green.500'} _hover={{backgroundColor : 'green.600'}} color={'white'} _dark={{backgroundColor : 'green.400'}} onClick={scrollToTop} mt={'6'} >Back to top</Button>
+                <Box display={'flex'} justifyContent={'center'} gap={'1'} alignItems={'center'} mb={'2'} mt={'4'}>
+                    <Text fontSize={'xs'}>Made with <Link color={'blue.500'} fontSize={'sm'} href={'https://chakra-ui.com/'} isExternal>
+                        Chakra Ui
+                    </Link>
+                    </Text>
+                    <BiLinkExternal/>
+                </Box>
+                <Text fontSize={'xs'} mb={'2'} display={'flex'} justifyContent={'center'} alignItems={'center'} gap={'2'} mt={'4'}><AiOutlineWarning/> this site is currently under development</Text>
             </Box>
 
         </Box>
