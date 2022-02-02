4ebb0663005cce63a39dfec8b9ed40416cc28e3f
abdul waheed <a.waheed3738@gmail.com>
2022-02-02 21:44:53 +0500

    building footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,22 +1,23 @@
-import {Box, Flex, Heading, Text,Link} from "@chakra-ui/react";
+import {Box, Flex, Heading, Text, Link, Button} from "@chakra-ui/react";
 import {AiFillGithub, AiFillLinkedin, AiOutlineTwitter} from "react-icons/ai";
 import {BiLinkExternal} from "react-icons/bi";
 
 const Footer = () => {
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+        });
+    }
     return (
         <Box as={'footer'}>
-        <Flex justifyContent={'space-between'} pb={'6'}>
-            <Box>
-                <Heading fontSize={'2xl'} letterSpacing={'widest'}>A.Waheed</Heading>
-
-            </Box>
+        <Flex justifyContent={'center'} pb={'10'}>
             <Box display={'flex'} gridGap={'6'}>
                 <AiFillGithub size={'25'}/>
                 <AiOutlineTwitter size={'25'} />
                 <AiFillLinkedin size={'25'}/>
             </Box>
         </Flex>
-            <Box textAlign={'center'} pb={'6'}>
+            <Box textAlign={'center'} pb={'12'}>
                 <Text fontSize={'sm'} mb={'2'}>developed and designed by Abdul Waheed</Text>
                 <Box display={'flex'} justifyContent={'center'} gap={'1'} alignItems={'center'} mb={'2'}>
                 <Text fontSize={'xs'}>Made with <Link color={'blue.500'} fontSize={'sm'} href={'https://chakra-ui.com/'} isExternal>
@@ -26,6 +27,7 @@ const Footer = () => {
                    <BiLinkExternal/>
                 </Box>
                 <Text fontStyle={'italic'} color={'gray.500'} fontSize={'sm'}>© 2022,All rights reserved.</Text>
+                <Button bgColor={'green.500'} _hover={{backgroundColor : 'green.600'}} color={'white'} _dark={{backgroundColor : 'green.400'}} onClick={scrollToTop} mt={'6'} >Back to top</Button>
             </Box>
 
         </Box>
