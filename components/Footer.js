e72c2f88387b6ad27d7b4b4d558b00db85b7ac0e
abdul waheed <a.waheed3738@gmail.com>
2022-02-02 22:27:08 +0500

    building footer

--- components/Footer.js
+++ components/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
     return (
         <Box as={'footer'}>
         <Flex justifyContent={'center'} pb={'8'} mt={'4'}>
-                <Tooltip label="Under development" aria-label='A tooltip' bg={'red.400'} defaultIsOpen={true}>
+                <Tooltip label="Under development" aria-label='A tooltip' bg={'red.400'} defaultIsOpen={true} closeDelay={2500}>
             <Box display={'flex'} gridGap={'6'}>
 
                 <AiFillGithub size={'25'}/>
