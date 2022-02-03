7f7fc4848fa9a8b502ffed69260ac41340c3bba1
abdul waheed <a.waheed3738@gmail.com>
2022-02-03 13:11:42 +0500

    added animation with framer-motion

--- components/helpers/helpers.js
+++ components/helpers/helpers.js
@@ -2,23 +2,23 @@ import {motion} from "framer-motion";
 import {Box, Image} from "@chakra-ui/react";
 
 export const imageVariants = {
-    visible: { opacity: 1, x: 0, transition: { duration: 1.5 } },
+    visible: { opacity: 1, x: 0, transition: { duration: 1 } },
     hidden: { opacity: 0, x: 40 }
 };
 export const opacityVariants = {
-    visible: { opacity: 1, transition: { duration: 1.5 } },
+    visible: { opacity: 1, transition: { duration: 1 } },
     hidden: { opacity: 0,  }
 };
 export const outerimageVariants = {
-    visible: { opacity: 1, x: 0, transition: { duration: 1.5 } },
+    visible: { opacity: 1, x: 0, transition: { duration: 1 } },
     hidden: { opacity: 0, x: -40 }
 };
 export const outerboxVariants = {
-    visible: { opacity: 1, x: 0, transition: { duration: 1.5 } },
+    visible: { opacity: 1, x: 0, transition: { duration: 1 } },
     hidden: { opacity: 0, x: 40 }
 };
 export const boxVariants = {
-    visible: { opacity: 1, x: 0, transition: { duration: 1.5 } },
+    visible: { opacity: 1, x: 0, transition: { duration: 1 } },
     hidden: { opacity: 0, x: -40 }
 };
 export const MotionImage = motion(Image)
