fb63d8be198caefa013b2ee15781f60bc2f9ef7f
abdul waheed <a.waheed3738@gmail.com>
2022-02-04 20:48:21 +0500

    added pre-commit with husky to cache posts automatically

--- components/Search.js
+++ components/Search.js
@@ -17,12 +17,41 @@ const Search = () => {
             setSearchResults(results) } }
         getResults()
     }, [searchInput])
+    function useOuterClick(callback) {
+        const innerRef = useRef();
+        const callbackRef = useRef();
 
+        // set current callback in ref, before second useEffect uses it
+        useEffect(() => { // useEffect wrapper to be safe for concurrent mode
+            callbackRef.current = callback;
+        });
+
+        useEffect(() => {
+            document.addEventListener("click", handleClick);
+            return () => document.removeEventListener("click", handleClick);
+
+            // read most recent callback and innerRef dom node from refs
+            function handleClick(e) {
+                if (
+                    innerRef.current &&
+                    callbackRef.current &&
+                    !innerRef.current.contains(e.target)
+                ) {
+                    callbackRef.current(e);
+                }
+            }
+        }, []);
+
+        return innerRef;
+    }
+    const innerRef = useOuterClick(e => {
+       setSearchInput('')
+    });
       return (
          <Box display={'flex'} justifyContent={'center'}>
-             <form>
+             <form id="container" ref={innerRef}>
                  <FormLabel>Search</FormLabel>
-                 <Input value={searchInput} onChange={ (e) => {setSearchInput(e.target.value)}} width={['xxs','xs','md','lg']}  maxLength={255} />
+                 <Input  id={'search'} value={searchInput} onChange={ (e) => {setSearchInput(e.target.value)}} width={['xxs','xs','md','lg']}  maxLength={255} />
              </form>
              <SearchResults results={searchResults}/>
          </Box>
